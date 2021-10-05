a46e21344fb0b23f8d16583a5622602a0099158d
Moisito12* <mrenteriadev@outlook.com>
2021-10-04 20:27:42 -0700

    end of section 14

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
